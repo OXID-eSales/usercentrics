42091ff00c12f7bc71a5500fd297fa9f270440aa
Anton Fedurtsya <anton@fedurtsya.com>
2020-11-11 18:02:14 +0200

    PSCORE-6 PSCORE-34 Add translations for menu items
    DE translations will be adjusted in separate task - PSCORE-42

---
+++ views/admin/en/oxps_cookieconsent_admin_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$sLangName = 'English';
+
+$aLang = array(
+    // Charsets
+    'charset'                                              => 'UTF-8',
+
+    // Lang-Strings
+    'oxpscookieconsent'                                    => 'Cookie consent',
+    'oxps_cookieconsent_services'                          => 'Services',
+    'oxps_cookieconsent_scripts'                           => 'Scripts',
+
+);
