8b6b297ab2cacadb6bb7cd5d67af935ab37877a1
batwolf <marc.plotz@oxid-esales.com>
2020-11-18 15:40:07 +0100

    fix phpcs errors

--- views/admin/de/module_options.php
+++ views/admin/de/module_options.php
@@ -3,8 +3,8 @@
 $sLangName = "Deutsch";
 
 $aLang = [
-    "charset"                                       => "UTF-8",
-    'SHOP_MODULE_GROUP_usercentrics_advanced'       => 'Erweiterte Einstellungen',
-    'SHOP_MODULE_smartDataProtectorActive'          => 'Smart Data Protector Aktivieren',
-    'HELP_SHOP_MODULE_smartDataProtectorActive'     => 'Die Deaktivierung dieser Funktion könnte rechtliche Folgen haben',
+    "charset"                                   => "UTF-8",
+    'SHOP_MODULE_GROUP_usercentrics_advanced'   => 'Erweiterte Einstellungen',
+    'SHOP_MODULE_smartDataProtectorActive'      => 'Smart Data Protector Aktivieren',
+    'HELP_SHOP_MODULE_smartDataProtectorActive' => 'Die Deaktivierung dieser Funktion könnte rechtliche Folgen haben',
 ];
