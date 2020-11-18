3549f5d904378f62e2997f3b6dbc0e8579c88fdb
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2020-11-18 17:48:49 +0100

    PSCORE-24 psalm warning fixed

--- Tests/Unit/Service/ExampleTest.php
+++ Tests/Unit/Service/ExampleTest.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 namespace OxidProfessionalServices\Usercentrics\Tests\Unit\Service;
 
 use OxidEsales\Eshop\Core\Registry;
+use OxidProfessionalServices\Usercentrics\Core\ViewConfig;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
     public function testSmartDataProtectorActiveByDefault(): void
     {
+        /** @var ViewConfig $viewConfig */
         $viewConfig = Registry::get(\OxidEsales\Eshop\Core\ViewConfig::class);
        // $this->assertInstanceOf(JavaScriptRenderer::class, $viewConfig);
         $enabled = $viewConfig->isSmartDataProtectorActive();
