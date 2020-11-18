bd56e408d52dce0962815815257afb5c6dd8f32d
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2020-11-18 17:26:59 +0100

    PSCORE-24 update example test to have code coverage

--- Tests/Unit/Service/ExampleTest.php
+++ Tests/Unit/Service/ExampleTest.php
@@ -3,15 +3,16 @@ declare(strict_types=1);
 
 namespace OxidProfessionalServices\Usercentrics\Tests\Unit\Service;
 
-use OxidEsales\Eshop\Core\Config;
 use OxidEsales\Eshop\Core\Registry;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    public function testExample(): void
+    public function testSmartDataProtectorActiveByDefault(): void
     {
-        $config = Registry::get(Config::class);
-        $this->assertInstanceOf(Config::class, $config);
+        $viewConfig = Registry::get(\OxidEsales\Eshop\Core\ViewConfig::class);
+       // $this->assertInstanceOf(JavaScriptRenderer::class, $viewConfig);
+        $enabled = $viewConfig->isSmartDataProtectorActive();
+        $this->assertTrue($enabled);
     }
 }
