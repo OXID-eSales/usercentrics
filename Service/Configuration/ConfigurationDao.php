72b9c7ace18c9e6a60e593c11a3a9b5f4ab834a0
Anton Fedurtsya <anton@fedurtsya.com>
2020-11-18 15:28:22 +0200

    PSCORE-4 Splitting the ConfigurationDao methods

--- Service/Configuration/ConfigurationDao.php
+++ Service/Configuration/ConfigurationDao.php
@@ -27,46 +27,125 @@ class ConfigurationDao implements ConfigurationDaoInterface
      */
     public function getConfiguration(): Configuration
     {
-        $plainConfig = $this->storage->getData();
+        $scripts = $this->getScriptsConfiguration();
+        $services = $this->getServicesConfiguration();
 
-        $plainScripts = isset($plainConfig['scripts']) ? $plainConfig['scripts'] : [];
-        if (! is_array($plainScripts)) {
-            $plainScripts = [];
-        }
+        return new Configuration($scripts, $services);
+    }
+
+    /**
+     * @return Script[]
+     */
+    private function getScriptsConfiguration(): array
+    {
+        $plainConfig = $this->storage->getData();
+        $plainScripts = $this->getConfigTypeFromPlainData('scripts', $plainConfig);
 
         $scripts = [];
-        foreach ($plainScripts as $script) {
-            $script['path'] = $script['path'] ?? "";
-            $script['service'] = $script['service'] ?? "";
-            $scripts[] = new Script((string) $script['path'], (string) $script['service']);
+        /** @var string[] $scriptDataArray */
+        foreach ($plainScripts as $scriptDataArray) {
+            $scripts[] = $this->scriptFromArray($scriptDataArray);
         }
 
-        $plainServices = isset($plainConfig['services']) ? $plainConfig['services'] : [];
-        if (! is_array($plainServices)) {
-            $plainServices = [];
-        }
+        return $scripts;
+    }
+
+    /**
+     * @return Service[]
+     */
+    private function getServicesConfiguration(): array
+    {
+        $plainConfig = $this->storage->getData();
+        $plainServices = $this->getConfigTypeFromPlainData('services', $plainConfig);
 
         $services = [];
-        foreach ($plainServices as $service) {
-            $service['name'] = $service['name']  ?? "";
-            $service['id'] = $service['id'] ?? "";
-            $services[] = new Service((string) $service['name'], (string) $service['id']);
+        /** @var string[] $serviceDataArray */
+        foreach ($plainServices as $serviceDataArray) {
+            $services[] = $this->serviceFromArray($serviceDataArray);
         }
-        return new Configuration($scripts, $services);
+
+        return $services;
+    }
+
+    /**
+     * @param string $typeOfList its a key - scripts|services
+     * @param mixed[] $plainConfig
+     *
+     * @return mixed[]
+     */
+    private function getConfigTypeFromPlainData(string $typeOfList, array $plainConfig): array
+    {
+        $typeConfig = $plainConfig[$typeOfList] ?? [];
+
+        if (!is_array($typeConfig)) {
+            $typeConfig = [];
+        }
+
+        return $typeConfig;
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    private function scriptFromArray(array $data): Script
+    {
+        $path = $data['path'] ?? "";
+        $service = $data['service'] ?? "";
+        return new Script((string)$path, (string)$service);
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    private function serviceFromArray(array $data): Service
+    {
+        $serviceName = $data['name'] ?? "";
+        $serviceId = $data['id'] ?? "";
+        return new Service((string)$serviceName, (string)$serviceId);
     }
 
     public function putConfiguration(Configuration $configuration): void
     {
-        $scripts = $configuration->getScripts();
+        $plainConfig = [
+            'scripts' => $this->preparePlainScriptsArray($configuration->getScripts()),
+            'services' => $this->preparePlainServicesArray($configuration->getServices())
+        ];
+
+        $this->storage->putData($plainConfig);
+    }
+
+    /**
+     * Converts array of Services to plain array for further saving
+     *
+     * @param Script[] $scripts
+     *
+     * @return array
+     */
+    private function preparePlainScriptsArray(array $scripts): array
+    {
         $plainScripts = [];
+
         foreach ($scripts as $script) {
             $plainScripts[] = [
                 'service' => $script->getServiceId(),
                 'path' => $script->getPath()
             ];
         }
+
+        return $plainScripts;
+    }
+
+    /**
+     * Converts array of Services to plain array for further saving
+     *
+     * @param Service[] $services
+     *
+     * @return array
+     */
+    private function preparePlainServicesArray(array $services): array
+    {
         $plainServices = [];
-        $services = $configuration->getServices();
+
         foreach ($services as $service) {
             $plainServices[] = [
                 'name' => $service->getName(),
@@ -74,11 +153,6 @@ class ConfigurationDao implements ConfigurationDaoInterface
             ];
         }
 
-        $plainConfig = [
-            'scripts' => $plainScripts,
-            'services' => $plainServices
-        ];
-
-        $this->storage->putData($plainConfig);
+        return $plainServices;
     }
 }
