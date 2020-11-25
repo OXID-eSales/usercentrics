8ff9f6cc2ce2d20e1fdf5d08e12ec028bcbeb3bb
Anton Fedurtsya <anton@fedurtsya.com>
2020-11-25 14:57:23 +0200

    PSCORE-7 Add Usercentrics Config module for codeception
    It allows to access configuration from the test

---
+++ tests/Codeception/Acceptance/ScriptIncludeAdjustementCest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidProfessionalServices\Usercentrics\Tests\Codeception\Acceptance;
+
+use OxidEsales\Codeception\Page\Home;
+use OxidProfessionalServices\Usercentrics\DataObject\Configuration;
+use OxidProfessionalServices\Usercentrics\DataObject\Script;
+use OxidProfessionalServices\Usercentrics\DataObject\ScriptSnippet;
+use OxidProfessionalServices\Usercentrics\DataObject\Service;
+use OxidProfessionalServices\Usercentrics\Tests\Codeception\AcceptanceTester;
+
+use OxidProfessionalServices\Usercentrics\Tests\Codeception\Module\Config;
+
+final class ScriptIncludeAdjustementCest
+{
+    private $configBackup;
+
+    public function _before(AcceptanceTester $I, Config $configModule): void
+    {
+        $this->configBackup = $configModule->getConfiguration();
+        $this->prepareConfiguration($configModule);
+    }
+
+    public function _after(AcceptanceTester $I, Config $configModule): void
+    {
+        $configModule->putConfiguration($this->configBackup);
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     *
+     * @group sieg
+     */
+    public function scriptIncludeDecorated(AcceptanceTester $I)
+    {
+        $homePage = new Home($I);
+        $I->amOnPage($homePage->URL);
+        $I->waitForElement("//button[@id='uc-btn-accept-banner']");
+        $I->click("//button[@id='uc-btn-accept-banner']");
+
+        $I->canSeeElementInDOM("//script[@type='text/javascript'][@data-usercentrics='testcustomservice']");
+    }
+
+    /**
+     * Prepare some test configuration before tests
+     */
+    private function prepareConfiguration(Config $configModule)
+    {
+        $config = new Configuration(
+            [ //scripts
+                new Script('out/flow/src/js/libs/jquery.min.js', 'testcustomservice')
+            ],
+            [ //services
+                new Service('testcustomservice', 'testcustomservice')
+            ],
+            [ //snippets
+                new ScriptSnippet('123', 'testcustomservice')
+            ]
+        );
+        $configModule->putConfiguration($config);
+    }
+}
---
+++ tests/Codeception/Module/Config.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidProfessionalServices\Usercentrics\Tests\Codeception\Module;
+
+use Codeception\Lib\Interfaces\DependsOnModule;
+use OxidProfessionalServices\Usercentrics\DataObject\Configuration;
+use OxidProfessionalServices\Usercentrics\Service\Configuration\ConfigurationDao;
+use OxidProfessionalServices\Usercentrics\Service\Configuration\YamlStorage;
+
+/**
+ * Class Config
+ */
+class Config extends \Codeception\Module implements DependsOnModule
+{
+    protected $requiredFields = ['shop_path', 'config_file'];
+    protected $config = [
+        'shop_path' => '',
+        'config_file' => ''
+    ];
+
+    public function _depends()
+    {
+        return [];
+    }
+
+    private function getConfigManager():ConfigurationDao
+    {
+        $storage =  new YamlStorage(
+            $this->config['shop_path'],
+            $this->config['config_file']
+        );
+
+        return new ConfigurationDao($storage);
+    }
+
+    public function getConfiguration(): Configuration
+    {
+        $cm = $this->getConfigManager();
+        return $cm->getConfiguration();
+    }
+
+    public function putConfiguration(Configuration $configuration): void
+    {
+        $cm = $this->getConfigManager();
+        $cm->putConfiguration($configuration);
+    }
+}
