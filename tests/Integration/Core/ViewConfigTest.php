260bec8a72806a908d0d8a72b859fbb72c25d6b3
Anton Fedurtsya <anton@fedurtsya.com>
2020-12-01 18:24:37 +0200

    Remove testing library usage in ViewConfig test

--- tests/Integration/Core/ViewConfigTest.php
+++ tests/Integration/Core/ViewConfigTest.php
@@ -9,21 +9,24 @@ namespace OxidProfessionalServices\Usercentrics\Tests\Integration\Core;
 
 use OxidEsales\Eshop\Core\Registry;
 use OxidProfessionalServices\Usercentrics\Core\ViewConfig;
+use PHPUnit\Framework\TestCase;
 
 /**
  * Class ViewConfigTest
  * @package OxidProfessionalServices\Usercentrics\Tests\Integration\Core
  * @covers \OxidProfessionalServices\Usercentrics\Core\ViewConfig
  */
-class ViewConfigTest extends \OxidEsales\TestingLibrary\UnitTestCase
+class ViewConfigTest extends TestCase
 {
     /**
      * @dataProvider booleanProvider
      */
     public function testSmartDataProtectorActive(bool $setting): void
     {
+        $config = Registry::getConfig();
         /** @psalm-suppress InvalidScalarArgument fails because of wrong typehin in used oxid version */
-        $this->getConfig()->setConfigParam("smartDataProtectorActive", $setting);
+        $config->setConfigParam("smartDataProtectorActive", $setting);
+
         /** @var ViewConfig $viewConfig */
         $viewConfig = Registry::get(\OxidEsales\Eshop\Core\ViewConfig::class);
         // $this->assertInstanceOf(JavaScriptRenderer::class, $viewConfig);
@@ -33,6 +36,9 @@ class ViewConfigTest extends \OxidEsales\TestingLibrary\UnitTestCase
 
     public function booleanProvider(): array
     {
-        return [[true],[false]];
+        return [
+            [true],
+            [false]
+        ];
     }
 }
