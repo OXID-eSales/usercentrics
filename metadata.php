d2cccd4a0b0f7b11ab3ef6535318cbd82b6a8712
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2021-02-25 15:00:24 +0100

    new deeplink for usercentrics

--- metadata.php
+++ metadata.php
@@ -16,8 +16,8 @@ $aModule = [
         'de' => 'Die Usercentrics Consent Management Platform (CMP) ermöglicht Ihnen, Ihre Marketing- und Datenstrategie
                  mit rechtlichen Anforderungen in Einklang zu bringen.</p>
                  <h2>Registrieren Sie sich deshalb jetzt bei Usercentrics</h2>  
-                 <form target="_blank" method="GET" action="https://usercentrics.com/de/?partnerid=16967">               
-                     <input name="partnerid" value="16967">
+                 <form target="_blank" method="GET" action="https://usercentrics.com/de/preise/?partnerid=16967#business-paket">
+                     <input type="hidden" name="partnerid" value="16967">
                      <input style="background: #0A2; color: #fff; font-weight: bold;" type="submit" value="Jetzt registrieren">
                      <p>
                          Sollte ein anderer Mitarbeiter in Ihrem Unternehmen die Registrierung durchführen, bitte dabei zwingend die OXID Partner-ID 16967 angeben, um die Integration vollständig nutzen zu können. Zu diesem Zweck können Sie diesen Link weitergeben: https://usercentrics.com/de/?partnerid=16967        
@@ -27,8 +27,8 @@ $aModule = [
         'en' => 'The Usercentrics Consent Management Platform (CMP) enables you to harmonize your marketing and data 
                  strategy with legal requirements.</p>
                  <h2>Register now for Usercentrics</h2>  
-                 <form target="_blank" method="GET" action="https://usercentrics.com/?partnerid=16967">               
-                     <input name="partnerid" value="16967">
+                 <form target="_blank" method="GET" action="https://usercentrics.com/pricing/?partnerid=16967#business-package">               
+                     <input type="hidden" name="partnerid" value="16967">
                      <input style="background: #0A2; color: #fff; font-weight: bold;" type="submit" value="Register Now">
                      <p>
                          If another employee in your company registers, please make sure to enter the OXID partner ID 16967 in order to be able to fully use the integration. For that reason you can forward this link to them: https://usercentrics.com/?partnerid=16967
