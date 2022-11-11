1a2fe3534f5edd9194dff6fa0c91ce04beb3a10d
Anton Fedurtsya <anton@fedurtsya.com>
2022-11-11 14:46:04 +0200

    Add missing translations for twig version
    Temporary solution till there will be a way to have translations separately from themes
    
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

---
+++ views/admin_twig/de/module_options.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+$sLangName = "Deutsch";
+
+$aLang = [
+    "charset"                                               => "UTF-8",
+    'SHOP_MODULE_GROUP_usercentrics_main'                   => 'Usercentrics Integration',
+    'SHOP_MODULE_GROUP_usercentrics_advanced'               => 'Erweiterte Einstellungen',
+    'SHOP_MODULE_smartDataProtectorActive'                  => 'Smart Data Protector aktivieren',
+    'HELP_SHOP_MODULE_smartDataProtectorActive'             => 'Die Deaktivierung dieser Funktion könnte rechtliche Folgen haben.',
+    'SHOP_MODULE_smartDataProtectorDeactivateBlocking'      => 'Liste von Services, für welche der Smart Data Protector deaktiviert ist',
+    'HELP_SHOP_MODULE_smartDataProtectorDeactivateBlocking' => 'Weitere Informationen: <a target="_BLANK" href="https://docs.usercentrics.com/#/smart-data-protector?id=deactivate-smart-data-protector-for-specific-services">hier</a>',
+    'SHOP_MODULE_usercentricsId'                            => 'Usercentrics Script ID',
+    'SHOP_MODULE_usercentricsMode'                          => 'Usercentrics Modus und Version',
+    'SHOP_MODULE_usercentricsMode_CmpV1'                    => 'CMP Version 1',
+    'SHOP_MODULE_usercentricsMode_CmpV2'                    => 'CMP Version 2',
+    'SHOP_MODULE_usercentricsMode_CmpV2Legacy'              => 'CMP V2 für alte Browser',
+    'SHOP_MODULE_usercentricsMode_CmpV2Tcf'                 => 'TCF 2.0',
+    'SHOP_MODULE_usercentricsMode_CmpV2TcfLegacy'           => 'TCF 2.0 für alte Browser',
+    'SHOP_MODULE_usercentricsMode_Custom'                   => 'Usercentrics Script selbst einbinden',
+    'HELP_SHOP_MODULE_usercentricsMode'                     => 'Bitte wählen Sie hier den Modus aus, in dem Usercentrics läuft! Eine Erklärung der Versionen, Legacy Support und TCF Support finden Sie unter docs.usercentrics.com. Nutzen Sie die Option „Usercentrics Script selbst einbinden“ nur, wenn das Modul das Usercentrics Script nicht hinzufügen soll.',
+];
---
+++ views/admin_twig/en/module_options.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+$sLangName = "English";
+
+$aLang = [
+    "charset"                                    => "UTF-8",
+    'SHOP_MODULE_GROUP_usercentrics_main'        => 'Usercentrics Integration',
+    'SHOP_MODULE_GROUP_usercentrics_advanced'    => 'Advanced settings',
+    'SHOP_MODULE_smartDataProtectorActive'       => 'Activate Usercentrics Smart Data Protector',
+    'HELP_SHOP_MODULE_smartDataProtectorActive' => 'Disabling this function could have legal consequences.',
+    'SHOP_MODULE_smartDataProtectorDeactivateBlocking' => 'List of services to Disable blocking',
+    'HELP_SHOP_MODULE_smartDataProtectorDeactivateBlocking' => 'More information: <a target="_BLANK" href="https://docs.usercentrics.com/#/smart-data-protector?id=deactivate-smart-data-protector-for-specific-services">here</a>',
+    'SHOP_MODULE_usercentricsId'                 => 'Usercentrics Script ID',
+    'SHOP_MODULE_usercentricsMode'               => 'Usercentrics Mode and Version',
+    'SHOP_MODULE_usercentricsMode_CmpV1'         => 'CMP Version 1',
+    'SHOP_MODULE_usercentricsMode_CmpV2'         => 'CMP Version 2',
+    'SHOP_MODULE_usercentricsMode_CmpV2Legacy'   => 'CMP V2 for old Browsers',
+    'SHOP_MODULE_usercentricsMode_CmpV2Tcf'      => 'TCF 2.0',
+    'SHOP_MODULE_usercentricsMode_CmpV2TcfLegacy' => 'TCF 2.0 for old Browsers',
+    'SHOP_MODULE_usercentricsMode_Custom'        => 'Include usercentrics script by yourself',
+    'HELP_SHOP_MODULE_usercentricsMode'          => 'Please choose the mode for Usercentrics! A documentation about versions, legacy browser support and TCF support can be found at <a target="_blank" href="https://docs.usercentrics.com/">docs.usercentrics.com</a>. Choose "Include usercentrics script by yourself" if the module itself should not add the usercentrics script.',
+];
